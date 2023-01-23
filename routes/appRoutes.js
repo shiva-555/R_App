b913c63c68f9aa86dbe891695c7343af0995ec0e
sanketgadakh123 <sanketgadakh123@gmail.com>
2023-01-23 21:51:09 +0530

    minor changes

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -23,11 +23,11 @@ router.get('/appUsers/:userId', appController.getAppUser);
 /* Candidate Routes */
 router.get('/candidates', authorizeRoles('Recruiter', 'Admin', 'TA Manager', 'HR'), appController.getCandidates);
 
-router.post('/candidate', authorizeRoles('Recruiter', 'Admin'),
+router.post('/candidate', authorizeRoles('Recruiter','Admin', 'TA Manager', 'Admin'),
 //  validate({body: candidateSchema}), 
  appController.createCandidate);
-router.get('/candidate/:candidateId', authorizeRoles('Recruiter', 'Admin'), appController.getCandidate)
-router.put('/candidate/:candidateId', authorizeRoles('Recruiter', 'Admin'), appController.updateCandidate);
+router.get('/candidate/:candidateId',authorizeRoles('Recruiter', 'Admin', 'TA Manager', 'HR'), appController.getCandidate)
+router.put('/candidate/:candidateId',authorizeRoles('Recruiter', 'Admin', 'TA Manager', 'HR'), appController.updateCandidate);
 router.post('/uploadDocuments', upload.single('file'), appController.uploadDocuments);
 router.post('/candidate/:candidate_id/interview', authorizeRoles('Recruiter', 'Admin'), appController.scheduleInterview);
 router.put('/candidate/:candidate_id/interview/:interview_id', appController.updateInterview);
@@ -37,10 +37,10 @@ router.get('/candidateReport', appController.getCandidatesReport);
 
 /* Job Requisitions*/
 //! Need to Add H Code
-router.get('/jobRequisitions', authorizeRoles('Recruiter', 'Admin'),appController.getJobRequisitions);
+router.get('/jobRequisitions', authorizeRoles('Recruiter', 'Admin', 'TA Manager', 'HR'),appController.getJobRequisitions);
 // router.post('/jobRequisition', upload.single('file'), appController.createJobRequisition, authorizeRoles('Admin', 'Recruitement Manager', 'Hiring Manager'));
 // router.put('/jobRequisition/:job_id', upload.single('file'), appController.updateJobRequisition, authorizeRoles('Admin', 'Recruitement Manager', 'Hiring Manager'));
-router.get('/getAllJobRequisitions',authorizeRoles('Recruiter', 'Admin'), appController.getAllJobRequisitions)
+router.get('/getAllJobRequisitions',authorizeRoles('Recruiter', 'Admin', 'TA Manager', 'HR'), appController.getAllJobRequisitions)
 
 
 /* Dashboard*/
